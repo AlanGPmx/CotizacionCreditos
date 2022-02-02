c657056805cecdc05c8e30799163fb34b2ab3249
Alan Garduño <alansgp@outlook.com>
2022-02-02 15:26:46 -0600

    Products MVCR

---
+++ resources/views/application/products/index.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="h4 font-weight-bold">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
+
+    <div class="row">
+        <div class="col-6">
+            <h2>Tres</h2>
+        </div>
+        <div class="col-6">
+            <h2>Cuatro</h2>
+        </div>
+    </div>
+    
+</x-app-layout>
